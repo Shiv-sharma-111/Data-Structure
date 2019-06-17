f2e1c9321b9df7862bbe3a97fbc59e349f842cf1
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-06-17 21:26:26 +0530

    Hackerearth

---
+++ code/One_String_No_Trouble.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	string str;
+	getline(cin,str);
+	int count=0;
+	int n=str.size();
+	for(int i=0;i<2;i++)
+	{
+		for(int j=i;j<=(i+1);j++)
+		{
+			if(str[i]==str[j])
+			{
+				break;
+			}
+			count++
+		}
+	}
+}
