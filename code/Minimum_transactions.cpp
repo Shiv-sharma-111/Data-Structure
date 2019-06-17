bb1eef53a9858321b3e187c00b1bd6696ed47086
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-06-17 11:52:20 +0530

    hackerearth

---
+++ code/Minimum_transactions.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,m;
+	cin>>n>>m;
+	int arr[m],arr1[m],arr2[m];
+	for(int i=0;i<m;i++)
+	{
+		cin>>arr[i];
+		cin>>arr1[i];
+		cin>>arr2[i];	
+	}
+	int sum[m];
+	for(int i=0;i<m;i++)
+	{
+		sum[i]=0;
+	}
+	for(int i=0;i<m;i++)
+	{
+		if(arr[i]==(i+1))
+		{
+			sum[i]
+		}
+	}
+	
+}
