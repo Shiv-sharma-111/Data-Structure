b68f640cd1629cc853f49e9aa8b1cf3d6bdc48c4
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-06-21 10:12:32 +0530

    Update String_Game.cpp

--- code/String_Game.cpp
+++ code/String_Game.cpp
@@ -2,7 +2,27 @@
 using namespace std;
 int main()
 {
-	string str;
-	getline(cin,str);
-	int n=str.size()
+int t;
+scanf("%d",&t);
+while(t--)
+{
+string s;
+cin>>s;
+int a[26]={0};
+int size=s.length();
+int c=0;
+for(int i=0;i<size;i++)
+{
+a[s[i]-'a']++;
+}
+for(int i=0;i<26;i++)
+{
+if(a[i]!=0)
+c++;
+}
+if(c%2==0)
+printf("Player2\n");
+else
+printf("Player1\n");
+}
 }
