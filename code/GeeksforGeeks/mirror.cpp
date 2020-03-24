1dd9b1cb2c490215d41f91904bd85b9bb40f7c73
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2020-03-24 05:44:54 -0400

    Update mirror.cpp

--- code/GeeksforGeeks/mirror.cpp
+++ code/GeeksforGeeks/mirror.cpp
@@ -1,5 +1,15 @@
+/*
 
-/* Should convert tree to its mirror */
+Algorithm – Mirror(tree):
+
+(1)  Call Mirror for left-subtree    i.e., Mirror(left-subtree)
+(2)  Call Mirror for right-subtree  i.e., Mirror(right-subtree)
+(3)  Swap left and right subtrees.
+          temp = left-subtree
+          left-subtree = right-subtree
+          right-subtree = temp
+
+*/
 void mirror(Node* node) 
 {
     if(node==NULL)
