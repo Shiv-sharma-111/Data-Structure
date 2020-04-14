fc9687a2ac0b858164e12a48dc55b36bf38ba10d
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2020-04-14 04:10:07 -0400

    Create Print-adjacency-list.cpp

---
+++ code/GeeksforGeeks/Graph/Print-adjacency-list.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void edge(vector<int>graph[],int from,int to)
+{   
+    graph[from].push_back(to);
+    graph[to].push_back(from) ;
+    
+}
+int main()
+ {
+	//code
+	int t;
+	cin >> t;
+	for (int i = 0;i<t;i++)
+	{
+	    int v,e ;
+	    cin >> v >> e ;
+	    vector<int> graph[v] ;
+	    int a,b;
+	    for (int j  = 0;j<e ;j++)
+	    {
+	        
+	        cin >> a >> b;
+	        edge(graph,a,b);
+	    }
+	    
+	    for (int e =0;e<v;e++)
+	    {
+	        cout << e ;
+	        for (int f = 0;f<graph[e].size();f++)
+	        {   
+	            cout << "-> " << graph[e][f] ;
+	            
+	        }
+	        cout << '\n' ;
+	    }
+	    
+	}
+}
